69371b7ebc49cff093ff20a820aced6f192a7e8f
王鹏 <njucmwp@126.com>
2020-01-31 01:38:09 +0800

    work队列

---
+++ src/main/java/com/njucm/rabbitmq/work/Recv2.java
@@ -0,0 +1,29 @@
+package com.njucm.rabbitmq.work;
+
+import com.njucm.rabbitmq.util.ConnectionUtil;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Recv2 {
+    private final static String QUEUE_NAME = "work_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        // 设置每个消费者同时只能处理一条信息
+        channel.basicQos(1);
+        DefaultConsumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String msg = new String(body);
+                System.out.println(" [consumer_2] received: " + msg + "!");
+                channel.basicAck(envelope.getDeliveryTag(), false);
+            }
+        };
+
+        channel.basicConsume(QUEUE_NAME, false, consumer);
+    }
+}
--- src/main/java/com/njucm/rabbitmq/work/Send.java
+++ src/main/java/com/njucm/rabbitmq/work/Send.java
@@ -1,4 +1,27 @@
 package com.njucm.rabbitmq.work;
 
+import com.njucm.rabbitmq.util.ConnectionUtil;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Send {
+    private final static String QUEUE_NAME = "work_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        Connection connection = ConnectionUtil.getConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+        for (int i = 0; i < 50; i++) {
+            String message = "task .. " + i;
+            channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+            Thread.sleep(i * 2);
+        }
+        channel.close();
+        connection.close();
+    }
 }

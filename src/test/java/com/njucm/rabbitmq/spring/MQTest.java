209114c4df9176f63b2f4f630dbbc19ba4049dfe
王鹏 <njucmwp@126.com>
2020-01-31 03:54:32 +0800

    spring amqp

---
+++ src/test/java/com/njucm/rabbitmq/spring/MQTest.java
@@ -0,0 +1,23 @@
+package com.njucm.rabbitmq.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = Application.class)
+@RunWith(SpringRunner.class)
+public class MQTest {
+
+    @Autowired
+    private AmqpTemplate amqpTemplate;
+
+    @Test
+    public void testSend() throws InterruptedException {
+        String msg = "hello, Spring boot amqp";
+        amqpTemplate.convertAndSend("spring.test.exchange", "a.b", msg);
+        Thread.sleep(10000);
+    }
+}
